836fe50376663fc296c26d1b23bf62281fddd236
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-08-12 10:23:58 -0400

    Add unit test for the ByteConverter class

---
+++ app/src/test/java/com/christianv07/dev/speedy/ByteConverterTest.kt
@@ -0,0 +1,37 @@
+package com.christianv07.dev.speedy
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class ByteConverterTest {
+    private val byteConverter = ByteConverter()
+
+    @Test
+    fun toKB_ConvertDouble_ReturnAsKilobytes() {
+        val expected = 8192.0
+        val actual = byteConverter.toKB(1.0)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun toMB_ConvertDouble_ReturnAsMegabytes() {
+        val expected = 8388608.0
+        val actual = byteConverter.toMB(1.0)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun toGB_ConvertDouble_ReturnAsGigabytes() {
+        val expected = 8589934592.0
+        val actual = byteConverter.toGB(1.0)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun toTB_ConvertDouble_ReturnAsTerabytes() {
+        val expected = 8.796093022208E12
+        val actual = byteConverter.toTB(1.0)
+        assertEquals(expected, actual)
+    }
+
+}
