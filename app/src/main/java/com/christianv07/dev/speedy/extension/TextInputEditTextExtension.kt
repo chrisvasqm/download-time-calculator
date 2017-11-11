0aa24d8379cd0407a0065e0c2df46270c2bc6f89
Christian Vasquez <christianvasquezmelo@gmail.com>
2017-11-11 07:24:14 -0400

    Fix #27

--- app/src/main/java/com/christianv07/dev/speedy/extension/TextInputEditTextExtension.kt
+++ app/src/main/java/com/christianv07/dev/speedy/extension/TextInputEditTextExtension.kt
@@ -3,14 +3,14 @@ package com.christianv07.dev.speedy.extension
 import android.support.design.widget.TextInputEditText
 
 fun TextInputEditText.getDoubleOrZero(): Double {
-    if (this.text.toString().isBlank())
+    if (this.text.toString().isBlank() || this.text.toString() == ".")
         return 0.0
 
     return this.text.toString().toDouble()
 }
 
 fun TextInputEditText.getIntOrZero(): Int {
-    if (this.text.toString().isBlank())
+    if (this.text.toString().isBlank() || this.text.toString() == ".")
         return 0
 
     return this.text.toString().toInt()
