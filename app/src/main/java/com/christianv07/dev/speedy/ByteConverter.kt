1f379944600cfeec376a73590ab7ecc4b0a4d999
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-03-17 16:13:31 -0400

    Refactor code

--- app/src/main/java/com/christianv07/dev/speedy/ByteConverter.kt
+++ app/src/main/java/com/christianv07/dev/speedy/ByteConverter.kt
@@ -1,21 +1,23 @@
 package com.christianv07.dev.speedy
 
+import kotlin.math.pow
+
 class ByteConverter {
 
     fun toKB(number: Double): Double {
-        return number * Math.pow(2.0, 13.0)
+        return number * 2.0.pow(13.0)
     }
 
     fun toMB(number: Double): Double {
-        return number * Math.pow(2.0, 23.0)
+        return number * 2.0.pow(23.0)
     }
 
     fun toGB(number: Double): Double {
-        return number * Math.pow(2.0, 33.0)
+        return number * 2.0.pow(33.0)
     }
 
     fun toTB(number: Double): Double {
-        return number * Math.pow(2.0, 43.0)
+        return number * 2.0.pow(43.0)
     }
 
 }
