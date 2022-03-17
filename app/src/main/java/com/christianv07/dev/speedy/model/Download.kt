a709199e13c75b1d4aa8aad32012d70e8c696a87
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-03-17 16:07:21 -0400

    Add default values

--- app/src/main/java/com/christianv07/dev/speedy/model/Download.kt
+++ app/src/main/java/com/christianv07/dev/speedy/model/Download.kt
@@ -1,7 +1,7 @@
 package com.christianv07.dev.speedy.model
 
 data class Download(
-        val size: Double,
-        val speed: Double,
-        val progress: Double = 0.0
+    val size: Double = 0.0,
+    val speed: Double = 0.0,
+    val progress: Double = 0.0
 )
