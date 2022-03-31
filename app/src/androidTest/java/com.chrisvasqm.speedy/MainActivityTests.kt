599f1b387c191918ff25e9570047bfff2749a00c
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-03-31 19:44:42 -0400

    Add clear button test

--- app/src/androidTest/java/com.chrisvasqm.speedy/MainActivityTests.kt
+++ app/src/androidTest/java/com.chrisvasqm.speedy/MainActivityTests.kt
@@ -2,18 +2,22 @@ package com.chrisvasqm.speedy
 
 import android.view.View
 import android.widget.SeekBar
+import androidx.appcompat.widget.AppCompatSeekBar
 import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.espresso.matcher.ViewMatchers.*
 import com.christianv07.dev.speedy.R
 import com.christianv07.dev.speedy.ui.main.MainActivity
+import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers.hasToString
 import org.junit.Test
 
 
@@ -53,7 +57,7 @@ class MainActivityTests {
     private fun withProgress(progress: Int): ViewAction {
         return object : ViewAction {
             override fun getConstraints(): Matcher<View> {
-                return ViewMatchers.isAssignableFrom(SeekBar::class.java)
+                return isAssignableFrom(SeekBar::class.java)
             }
 
             override fun getDescription(): String {
@@ -62,6 +66,58 @@ class MainActivityTests {
 
             override fun perform(uiController: UiController?, view: View?) {
                 (view as SeekBar).progress = progress
+            }
+        }
+    }
+
+    @Test
+    fun clearButton_WhenEveryFieldHasBeenChanged_ReturnThemToDefaultState() {
+        launchActivity<MainActivity>().use {
+            onView(withId(R.id.editFilesize))
+                .perform(typeText("1"))
+
+            onView(withId(R.id.spinnerFilesize))
+                .perform(click())
+            onData(hasToString("MB")).perform(click())
+
+            onView(withId(R.id.editEstimatedSpeed))
+                .perform(typeText("1"))
+
+            onView(withId(R.id.spinnerEstimatedspeed))
+                .perform(click())
+            onData(hasToString("MB/s")).perform(click())
+
+            onView(withId(R.id.seekbarDownloaded))
+                .perform(withProgress(50))
+
+            onView(withId(R.id.btnClear))
+                .perform(click())
+
+            onView(withId(R.id.editFilesize))
+                .check(matches(withText("")))
+
+            onView(withId(R.id.spinnerFilesize))
+                .check(matches(withSpinnerText("KB")))
+
+            onView(withId(R.id.editEstimatedSpeed))
+                .check(matches(withText("")))
+
+            onView(withId(R.id.spinnerEstimatedspeed))
+                .check(matches(withSpinnerText("KB/s")))
+
+            onView(withId(R.id.seekbarDownloaded))
+                .check(matches(withSeekbarProgress(0)))
+        }
+    }
+
+    private fun withSeekbarProgress(expectedProgress: Int): Matcher<View?> {
+        return object : BoundedMatcher<View?, AppCompatSeekBar>(AppCompatSeekBar::class.java) {
+            override fun describeTo(description: Description) {
+                // Code block not needed
+            }
+
+            override fun matchesSafely(seekBar: AppCompatSeekBar): Boolean {
+                return seekBar.progress == expectedProgress
             }
         }
     }
